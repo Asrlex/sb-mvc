8bba411c76a2b0cdeb48b3fcf6b247421db5a6e2
Asrlex <alexsruc@hotmail.com>
2025-09-25 16:50:12 +0200

    base cron

---
+++ src/main/java/dev/api/springmvc/config/SchedulerConfig.java
@@ -0,0 +1,20 @@
+package dev.api.springmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar registrar) {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(5);
+		scheduler.setThreadNamePrefix("cron-thread-");
+		scheduler.initialize();
+		registrar.setTaskScheduler(scheduler);
+	}
+}
